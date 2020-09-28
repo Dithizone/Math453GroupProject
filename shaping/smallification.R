965006f211f489dc3e6237506233366e43078ba5
Peter Madsen <peter.madsen784@myci.csuci.edu>
2020-09-28 13:56:47 -0700

    Started initial script files

---
+++ shaping/smallification.R
@@ -0,0 +1,22 @@
+# This script will smallify the original data by dropping
+# columns or unwanted years, ages, etc.
+
+# Then it will save each produced data set into unique
+# *.csv files so we have a beautiful paper trail of all
+# the changes we make.
+
+# If, for example, we want a histogram of Housing Price
+# Opinion vs. Gender, we only need those two columns. So 
+# we should create it and save the result in, like, 
+# `house_opinion_vs_gender_2017.csv`
+
+# Useful shortcuts: CTRL+S saves, CTRL+ALT+R runs the
+# entire script. (The run button above just runs the 
+# highlighted region or a single line)
+
+source("ThingsWeDoALot.R")
+
+print("This will be a cool script when we get to it!")
+
+test_function("Peter")
+
