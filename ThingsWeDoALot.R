a5d31fa8996225cce6ba65c91c4dc165d8f9f77d
Peter Madsen <peter.madsen784@myci.csuci.edu>
2020-10-07 19:40:01 -0700

    Changed from script-based to Paper Trail.Rmd

--- ThingsWeDoALot.R
+++
@@ -1,9 +0,0 @@
-# This will be a script filled with all the functions
-# we do over and over again, so we could just, like, 
-# call `MakeHistogram(the_data)` instead of the long-
-# winded `ggplot() + geom_hist() + legend()` stuff 
-# all the time.
-
-test_function <- function(testname) {
-  cat("Hi! This is the test function, and your name is ", testname, "!", sep="")
-}
